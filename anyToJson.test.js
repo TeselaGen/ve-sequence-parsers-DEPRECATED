bddf1b4bcb1f075559ec83fa99b83c3f4756c15d
Thomas Rich <trich1230@gmail.com>
2016-05-10 15:32:44 -0700

    updating file names

---
+++ anyToJson.test.js
@@ -0,0 +1,85 @@
+//this test takes a sequence represented in several different file types and
+//makes sure that they give the same results regardless (for fields that make sense)
+var anyToJson = require('../parsers/anyToJson.js');
+var fs = require('fs');
+var path = require("path");
+var async = require('async');
+var assert = require('assert');
+var chai = require('chai');
+chai.use(require('chai-things'));
+chai.should();
+
+describe('anyToJson', function(argument) {
+    it('parses the pBbE0c-RFP plasmid represented in various filetypes to the same end result', function(done) {
+        var options = {
+            fastaFilePath: "pBbE0c-RFP.fasta",
+            genbankFilePath: 'pBbE0c-RFP.gb',
+            sbolFilePath: 'pBbE0c-RFP.xml',
+        };
+        runTest(done, options);
+    });
+    it('parses the pBbS0c-RFP plasmid represented in various filetypes to the same end result', function(done) {
+        var options = {
+            fastaFilePath: "pBbS0c-RFP.fasta",
+            genbankFilePath: 'pBbS0c-RFP.gb',
+            sbolFilePath: 'pBbS0c-RFP.xml',
+        };
+        runTest(done, options);
+    });
+});
+
+function runTest(done, options) {
+    var fastaResult;
+    var genbankResult;
+    var sbolXMLResult;
+    async.series([
+            function(done) {
+                var string = fs.readFileSync(path.join(__dirname, './testData/', options.fastaFilePath), "utf8");
+                anyToJson(string, function(result) {
+                    fastaResult = result;
+                    done();
+                }, {fileName: options.fastaFilePath, isProtein: false});
+            },
+            function(done) {
+                var string = fs.readFileSync(path.join(__dirname, './testData/', options.genbankFilePath), "utf8");
+                anyToJson(string, function(result) {
+                    genbankResult = result;
+                    done();
+                }, {fileName: options.genbankFilePath, isProtein: false});
+            },
+
+            function(done) {
+                var string = fs.readFileSync(path.join(__dirname, './testData/', options.sbolFilePath), "utf8");
+                anyToJson(string, function(result) {
+                    sbolXMLResult = result;
+                    done();
+                }, {fileName: options.sbolFilePath, isProtein: false});
+            },
+        ],
+        function() {
+            //fasta to genbank check
+            fastaResult[0].parsedSequence.sequence.should.equal(genbankResult[0].parsedSequence.sequence);
+
+            sbolXMLResult[0].parsedSequence.features.length.should.equal(genbankResult[0].parsedSequence.features.length);
+            // sbolXMLResult[0].parsedSequence.circular.should.equal(genbankResult[0].parsedSequence.circular);
+            sbolXMLResult[0].parsedSequence.sequence.toLowerCase().should.equal(genbankResult[0].parsedSequence.sequence.toLowerCase());
+            //sbolXml to genbank check
+            //can't make checks for circularity because sbol sequences are assumed to be linear
+            // assert(sbolXMLResult[0].parsedSequence.circular === genbankResult[0].parsedSequence.circular);
+            sbolXMLResult[0].parsedSequence.features.forEach(function(feat1) {
+                assert(genbankResult[0].parsedSequence.features.filter(function(feat2) {
+                    //can't make checks for start or end because features are split on the origin in sbol
+                    if (feat1.name === feat2.name) {
+                        if (feat1.start === 0 && feat1.end === 0) {
+                            return true;
+                        }
+                        if (feat1.start === feat2.start && feat1.end === feat2.end) {
+                            return true;
+                        }
+                    }
+                }).length);
+            });
+            done();
+        }
+    );
+}
