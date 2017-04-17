6515ed134eafb314cda8c39e90a877bfee53f4d1
Taoh Green <greenmachine777@gmail.com>
2017-04-17 13:31:31 -0700

    can add an array of tags to sequence genbank parts

--- test/genbankToJson.test.js
+++ test/genbankToJson.test.js
@@ -236,6 +236,40 @@ describe('genbankToJson tests', function() {
             done();
         }, {fileName: 'CCR5_multifrag_insert1.gb'});
     });
+    it('parses a .gb file with tags on parts', function(done) {
+        var string = fs.readFileSync(path.join(__dirname, './testData/genbank/gbFileWithTagsOnParts.gb'), "utf8");
+        genbankToJson(string, function(result) {
+           result.should.be.an('array');
+            result[0].success.should.be.true;
+            result[0].parsedSequence.features.should.include.something.that.deep.equals({
+                notes: {
+                    pragma: ['Teselagen_Part'],
+                    preferred3PrimeOverhangs: [''],
+                    preferred5PrimeOverhangs: [''],
+                    tag: ['blue', 'red']
+                },
+                name: 'pS8c-gfpuv',
+                start: 1238,
+                end: 1234,
+                type: 'misc_feature',
+                strand: 1
+            });
+            result[0].parsedSequence.features.should.include.something.that.deep.equals({
+                notes: {
+                    pragma: ['Teselagen_Part'],
+                    preferred3PrimeOverhangs: [''],
+                    preferred5PrimeOverhangs: [''],
+                    tag: ['red', 'green']
+                },
+                name: 'pS8c-gfpuv_sig_pep_vector_backbone',
+                start: 1238,
+                end: 1234,
+                type: 'misc_feature',
+                strand: 1
+            });
+            done();
+        });
+    });
 
 });
 // var string = fs.readFileSync(path.join(__dirname, '../../../..', './testData/genbank (JBEI Private)/46.gb'), "utf8");
