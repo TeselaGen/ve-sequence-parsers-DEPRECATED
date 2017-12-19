a2cd6c89d6e178209a5676e5e0fd9279e13a519b
Thomas Rich <tnrich@teselagen.com>
2017-12-18 14:05:37 -1000

    adding test to make sure multi-location features get parsed correctly, fixing undefined variable issue

--- src/parsers/utils/convertOldSequenceDataToNewDataType.js
+++ src/parsers/utils/convertOldSequenceDataToNewDataType.js
@@ -3,10 +3,10 @@ module.exports = function convertToNewDataType(oldTeselagenJson) {
   oldTeselagenJson.features.forEach(function(feature) {
     if (feature.locations[0]) {
       if (feature.locations.length > 1) {
-        for (var i = 1; i < feature.locations.length; i++) { //start at index 1, not 0!
+        for (let i = 1; i < feature.locations.length; i++) { //start at index 1, not 0!
           //for every location except for the first one, 
-          var location = feature.locations[i];
-          clonedFeature = JSON.parse(JSON.stringify(feature));
+          const location = feature.locations[i];
+          let clonedFeature = JSON.parse(JSON.stringify(feature));
           clonedFeature.start = location.start;
           clonedFeature.end = location.end;
           delete clonedFeature.locations; //This array is no longer used to get start and end bp and doesn't need to be in db
