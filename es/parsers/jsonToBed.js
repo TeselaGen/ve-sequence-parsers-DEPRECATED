7e0582bc58ce81d703e5e869e65887fd12a468cd
Thomas Rich <tnrich@teselagen.com>
2018-07-18 14:18:35 -0700

    adding built files

--- es/parsers/jsonToBed.js
+++ es/parsers/jsonToBed.js
@@ -1,11 +1,6 @@
 var _require = require('ve-sequence-utils'),
     tidyUpSequenceData = _require.tidyUpSequenceData;
 
-var _require2 = require('lodash'),
-    each = _require2.each;
-
-var fs = require('fs');
-
 function jsonToBed(jsonSequence, options) {
   var cleanedData = tidyUpSequenceData(jsonSequence);
   var name = cleanedData.name,
@@ -13,31 +8,26 @@ function jsonToBed(jsonSequence, options) {
       size = cleanedData.size,
       description = cleanedData.description,
       circular = cleanedData.circular;
-  // const { name, features } = cleanedData[0].parsedSequence;
 
   options = options || {};
 
   var sequenceNameToMatchFasta = "";
-  sequenceNameToMatchFasta += '>' + (name || "Untitled Sequence") + '|';
+  sequenceNameToMatchFasta += (name || "Untitled Sequence") + "|";
   sequenceNameToMatchFasta += "|" + size;
   sequenceNameToMatchFasta += description ? "|" + description : "";
   sequenceNameToMatchFasta += "|" + (circular ? "circular" : "linear");
   var outString = "";
-  outString += 'track name="' + sequenceNameToMatchFasta + '" description="' + name + ' Annotations" itemRgb="On"\n';
+  outString += "track name=\"" + sequenceNameToMatchFasta + "\" description=\"" + name + " Annotations\" itemRgb=\"On\"\n";
 
   features.forEach(function (feat) {
-    // options.reformatSeqName = options.reformatSeqName === false ? false : true;
     var start = feat.start,
         end = feat.end,
-        name = feat.name,
         type = feat.type,
         forward = feat.forward;
     // chromStart is 0-based, chromEnd of the BED file format is not included in the feature
 
-    outString += sequenceNameToMatchFasta + '\t' + start + '\t' + (end + 1) + '\t' + type + '\t1000\t' + (forward ? "+" : "-") + '\t' + start + '\t' + (end + 1) + '\t65,105,225\n';
+    outString += sequenceNameToMatchFasta + "\t" + start + "\t" + (end + 1) + "\t" + type + "\t1000\t" + (forward ? "+" : "-") + "\t" + start + "\t" + (end + 1) + "\t65,105,225\n";
   });
-  // const filePath = "./src/parsers/AcsBmutJsonToBed.bed";
-  // fs.writeFileSync(filePath, outString)
   return outString;
 }
 
