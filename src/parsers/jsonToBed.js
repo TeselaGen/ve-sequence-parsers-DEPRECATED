ade525878e9dce91a0630312ee990dc47cc29365
Thomas Rich <tnrich@teselagen.com>
2018-07-18 14:11:41 -0700

    fixing issue with jsonToBed requiring fs module

--- src/parsers/jsonToBed.js
+++ src/parsers/jsonToBed.js
@@ -1,11 +1,8 @@
 const { tidyUpSequenceData } = require('ve-sequence-utils');
-const { each } = require('lodash');
-const fs = require('fs');
 
 function jsonToBed(jsonSequence, options) {
   const cleanedData = tidyUpSequenceData(jsonSequence);
   const { name, features, size, description, circular } = cleanedData;
-  // const { name, features } = cleanedData[0].parsedSequence;
   options = options || {};
 
   let sequenceNameToMatchFasta = "";
@@ -17,13 +14,10 @@ function jsonToBed(jsonSequence, options) {
   outString += `track name="${sequenceNameToMatchFasta}" description="${name} Annotations" itemRgb="On"\n`
 
   features.forEach(function(feat) {
-    // options.reformatSeqName = options.reformatSeqName === false ? false : true;
-    const {start, end, name, type, forward} = feat
+    const {start, end, type, forward} = feat
     // chromStart is 0-based, chromEnd of the BED file format is not included in the feature
     outString+=`${sequenceNameToMatchFasta}\t${start}\t${end + 1}\t${type}\t1000\t${forward ? "+" : "-"}\t${start}\t${end + 1}\t65,105,225\n`
   })
-  // const filePath = "./src/parsers/AcsBmutJsonToBed.bed";
-  // fs.writeFileSync(filePath, outString)
   return outString;
 }
 
