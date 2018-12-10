fc3da469446abb41bc273753176e39947baf9262
tnrich <tnrich@teselagen.com>
2018-12-10 12:08:26 -0800

    adding built files

--- es/parsers/jsonToGenbank.js
+++ es/parsers/jsonToGenbank.js
@@ -1,9 +1,11 @@
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
 /* eslint-disable no-var*/
+var _require = require("lodash"),
+    cloneDeep = _require.cloneDeep,
+    map = _require.map,
+    each = _require.each;
 
-var each = require("lodash/each");
-var map = require("lodash/map");
 var nameUtils = require("./utils/NameUtils.js");
 var StringUtil = {
   /** Trims white space at beginning and end of string
@@ -61,10 +63,10 @@ function cutUpStr(val, start, end) {
   return val.slice(start, end);
 }
 
-module.exports = function (serSeq, options) {
+module.exports = function (_serSeq, options) {
   options = options || {};
   options.reformatSeqName = options.reformatSeqName !== false;
-
+  var serSeq = cloneDeep(_serSeq);
   if (!serSeq) return false;
 
   try {
