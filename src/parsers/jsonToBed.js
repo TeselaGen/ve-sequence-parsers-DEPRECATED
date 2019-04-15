62a5084deec1d99deef5def00e94d22dfa6460b6
tiffanydai <tiffany.dai@teselagen.com>
2019-04-15 16:50:11 -0700

    adding strand to jsonToBed

--- src/parsers/jsonToBed.js
+++ src/parsers/jsonToBed.js
@@ -14,9 +14,9 @@ function jsonToBed(jsonSequence, options) {
   outString += `track name="${sequenceNameToMatchFasta}" description="${name} Annotations" itemRgb="On"\n`
 
   features.forEach(function(feat) {
-    const {start, end, type, forward} = feat
+    const {start, end, type, forward, strand} = feat
     // chromStart is 0-based, chromEnd of the BED file format is not included in the feature
-    outString+=`${sequenceNameToMatchFasta}\t${start}\t${end + 1}\t${type}\t1000\t${forward ? "+" : "-"}\t${start}\t${end + 1}\t65,105,225\n`
+    outString+=`${sequenceNameToMatchFasta}\t${start}\t${end + 1}\t${type}\t1000\t${forward || strand === 1 ? "+" : "-"}\t${start}\t${end + 1}\t65,105,225\n`
   })
   return outString;
 }
