b07935fd2f50ff6c3d551d90be1db77001f5eb37
tiffanydai <tiffany.dai@teselagen.com>
2019-04-18 23:47:08 -0700

    adding option to override seq name in jsonToBed

--- src/parsers/jsonToBed.js
+++ src/parsers/jsonToBed.js
@@ -1,4 +1,4 @@
-import { tidyUpSequenceData } from 've-sequence-utils';
+import { tidyUpSequenceData } from "ve-sequence-utils";
 
 function jsonToBed(jsonSequence, options) {
   const cleanedData = tidyUpSequenceData(jsonSequence);
@@ -9,15 +9,19 @@ function jsonToBed(jsonSequence, options) {
   sequenceNameToMatchFasta += `${name || "Untitled Sequence"}|`;
   sequenceNameToMatchFasta += "|" + size;
   sequenceNameToMatchFasta += description ? "|" + description : "";
-  sequenceNameToMatchFasta += "|" + (circular ?  "circular" : "linear");
+  sequenceNameToMatchFasta += "|" + (circular ? "circular" : "linear");
+  const sequenceNameToUse = options.sequenceName || sequenceNameToMatchFasta;
   let outString = "";
-  outString += `track name="${sequenceNameToMatchFasta}" description="${name} Annotations" itemRgb="On"\n`
+  outString += `track name="${sequenceNameToUse}" description="${name} Annotations" itemRgb="On"\n`;
 
   features.forEach(function(feat) {
-    const {start, end, type, forward, strand} = feat
+    const { start, end, type, forward, strand } = feat;
     // chromStart is 0-based, chromEnd of the BED file format is not included in the feature
-    outString+=`${sequenceNameToMatchFasta}\t${start}\t${end + 1}\t${type}\t1000\t${forward || strand === 1 ? "+" : "-"}\t${start}\t${end + 1}\t65,105,225\n`
-  })
+    outString += `${sequenceNameToMatchFasta}\t${start}\t${end +
+      1}\t${type}\t1000\t${
+      forward || strand === 1 ? "+" : "-"
+    }\t${start}\t${end + 1}\t65,105,225\n`;
+  });
   return outString;
 }
 
