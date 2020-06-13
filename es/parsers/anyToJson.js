84941d8fb32478deedf6f6fb6e67616e0a7aabbf
Thomas Heinrichs <thheinrichs@outlook.de>
2020-06-13 15:55:45 +0200

    Replace callback with Function in function comments

--- es/parsers/anyToJson.js
+++ es/parsers/anyToJson.js
@@ -178,7 +178,7 @@ import gffToJson from "./gffToJson";
  * takes in file content string and its file name and determines what parser it needs to be sent to.
  * The file is parsed to our old JSON schema and after it goes through an intermediate step where we convert that json to our new schema
  * @param  {string} fileContentString content of the file as a string
- * @param  {callback} onFileParsed    //tnr: fill this out
+ * @param  {Function} onFileParsed    //tnr: fill this out
  */
 import addPromiseOption from "./utils/addPromiseOption";
 
